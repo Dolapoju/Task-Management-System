025265a32b3f463366a4cd5d0e43e9f5a3e40e41
dolapoju <dolapoju2@gmail.com>
2026-01-29 12:56:08 +0100

    prettier

--- scripts/taskhandler.js
+++ scripts/taskhandler.js
@@ -1,116 +1,103 @@
-
 //DOM
-const form=document.querySelector('.add-task');
-const noTaskScreen= document.querySelector('.no-tasks');
-const completedTasks= document.querySelector('#completed-tasks');
-const submitted = document.querySelector('#up-arrow');
+const form = document.querySelector(".add-task");
+const noTaskScreen = document.querySelector(".no-tasks");
+const completedTasks = document.querySelector("#completed-tasks");
+const submitted = document.querySelector("#up-arrow");
 
 //Event
-const deleteevent= new CustomEvent('taskDeleted',{
-    bubbles:true
-})
+const deleteevent = new CustomEvent("taskDeleted", {
+  bubbles: true,
+});
 
 //Event Listeners
-form.addEventListener('submit',(event)=>{
-    event.preventDefault();
-    addTask();
-    spinup();
-
-})
-window.onload=()=>{
-    spinup();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addTask();
+  spinup();
+});
+window.onload = () => {
+  spinup();
 };
-submitted.onclick=()=>{
-    form.requestSubmit();
+submitted.onclick = () => {
+  form.requestSubmit();
 };
-completedTasks.addEventListener('change',(event)=>{
-    const hiddenkey=event.target.dataset.userId;
-    completedTaskhandler(hiddenkey);
-})
+completedTasks.addEventListener("change", (event) => {
+  const hiddenkey = event.target.dataset.userId;
+  completedTaskhandler(hiddenkey);
+});
 
-
-completedTasks.addEventListener('taskDeleted',(event)=>{
-    localStorage.removeItem(event.target.dataset.userId);
-    spinup();
-})
-
+completedTasks.addEventListener("taskDeleted", (event) => {
+  localStorage.removeItem(event.target.dataset.userId);
+  spinup();
+});
 
 //functions
-function addTask(){
-    const task= {
-        content:`${form.input.value}`,
-        isComplete:false,
-        category:"none"
-    }
-    let isUnique=false;
-    let id;
-    while(!isUnique){
-        isUnique=randomID()[0];
-        id=randomID()[1];
-    }
+function addTask() {
+  const task = {
+    content: `${form.input.value}`,
+    isComplete: false,
+    category: "none",
+  };
+  let isUnique = false;
+  let id;
+  while (!isUnique) {
+    isUnique = randomID()[0];
+    id = randomID()[1];
+  }
 
-    localStorage.setItem(id,JSON.stringify(task))
-
-
-
+  localStorage.setItem(id, JSON.stringify(task));
 }
 
-function spinup(){
-    if(localStorage.length==0){
-        noTaskScreen.style.display="block"
-        completedTasks.style.display="none";
-    }else{
-        noTaskScreen.style.display="none";
-        completedTasks.style.display="block";
-        completedTasks.innerHTML = '';
-        for(let i = 0; i < localStorage.length; i++) {
-            let key = localStorage.key(i);
-            const task= JSON.parse(localStorage.getItem(key))
-            if(task.isComplete==false){
-
-                completedTasks.innerHTML += `
+function spinup() {
+  if (localStorage.length == 0) {
+    noTaskScreen.style.display = "block";
+    completedTasks.style.display = "none";
+  } else {
+    noTaskScreen.style.display = "none";
+    completedTasks.style.display = "block";
+    completedTasks.innerHTML = "";
+    for (let i = 0; i < localStorage.length; i++) {
+      let key = localStorage.key(i);
+      const task = JSON.parse(localStorage.getItem(key));
+      if (task.isComplete == false) {
+        completedTasks.innerHTML += `
                 <div>
                     <input type="checkbox" data-user-id="${key}">
                     <span>${task.content}</span>
                     <svg data-user-id="${key}" id="delete-btn"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M232.7 69.9L224 96L128 96C110.3 96 96 110.3 96 128C96 145.7 110.3 160 128 160L512 160C529.7 160 544 145.7 544 128C544 110.3 529.7 96 512 96L416 96L407.3 69.9C402.9 56.8 390.7 48 376.9 48L263.1 48C249.3 48 237.1 56.8 232.7 69.9zM512 208L128 208L149.1 531.1C150.7 556.4 171.7 576 197 576L443 576C468.3 576 489.3 556.4 490.9 531.1L512 208z"/></svg>
 
                 </div>`;
-            }
-        }
-        try{
-            let deletebtn =document.querySelector('#delete-btn');
-            deletebtn.onclick=()=>{
-                deletebtn.dispatchEvent(deleteevent);
-            }
-        }catch{
-            //the try catch finally block prevents the code from crashing because there is no delete-btn in the DOM because everything has been deleted
-        }finally{
-            if(completedTasks.innerHTML==''){
-            noTaskScreen.style.display="block"
-            completedTasks.style.display="none";
-        }
-        }
-        
-        
-    }   
+      }
+    }
+    try {
+      let deletebtn = document.querySelector("#delete-btn");
+      deletebtn.onclick = () => {
+        deletebtn.dispatchEvent(deleteevent);
+      };
+    } catch {
+      //the try catch finally block prevents the code from crashing because there is no delete-btn in the DOM because everything has been deleted
+    } finally {
+      if (completedTasks.innerHTML == "") {
+        noTaskScreen.style.display = "block";
+        completedTasks.style.display = "none";
+      }
+    }
+  }
 }
 
-function completedTaskhandler(hiddenkey){
-    let task= JSON.parse(localStorage.getItem(hiddenkey)) 
-    task.isComplete=true;
-    localStorage.setItem(hiddenkey,JSON.stringify(task)) 
-    spinup();
+function completedTaskhandler(hiddenkey) {
+  let task = JSON.parse(localStorage.getItem(hiddenkey));
+  task.isComplete = true;
+  localStorage.setItem(hiddenkey, JSON.stringify(task));
+  spinup();
 }
-    
-function randomID(){
-    const num= Math.floor(Math.random()*100000);
-    if(localStorage.getItem(num)==null){
-        return [true,num];
-    }else{
-        return [false,num];
-    }
+
+function randomID() {
+  const num = Math.floor(Math.random() * 100000);
+  if (localStorage.getItem(num) == null) {
+    return [true, num];
+  } else {
+    return [false, num];
+  }
 }
 spinup();
-
-
-
